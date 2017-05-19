867bae5b17b62635970a5b79d80effc00dbfb864
Conner McCall <sloped@sloped.me>
2017-05-19 10:38:45 -0500

    fix missing express import

--- config/static_assets.js
+++ config/static_assets.js
@@ -1,7 +1,7 @@
 import webpack_config from './webpack.config.js';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpack from 'webpack';
-
+import express from 'express';
 module.exports = function(app) {
     if( process.env.NODE_ENV !== 'production' ) {
       app.use(webpackMiddleware(webpack(webpack_config), {
