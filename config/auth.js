44308ac529ef6da7cc0ba774f6bae3d3c506f915
Conner McCall <sloped@sloped.me>
2017-05-19 11:56:39 -0500

    update callback url to env var

--- config/auth.js
+++ config/auth.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
         function(accessToken, refreshToken, profile, done) {
                 User.findOrCreate({ googleId: profile.id }).then( (user) => {
