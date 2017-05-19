7748da67519dd441a52a2a2b1bf747f31cd97c87
Conner McCall <sloped@sloped.me>
2017-05-19 12:01:16 -0500

    forcing only clockwork.com emails

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import passport from 'passport';
 module.exports = function(app) {
     app.get('/auth/google',
-        passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/calendar.readonly'] })
+        passport.authenticate('google', { hd: 'clockwork.com', scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/calendar.readonly'] })
     );
     app.get('/auth/google/callback', 
         passport.authenticate('google', { failureRedirect: '/login' }),
