230e3e7073cf643a8f6dda292f149013d054516f
Conner McCall <sloped@sloped.me>
2017-05-23 15:13:41 -0500

    small tweaks, color updates

--- resources/js/vue-components/events/next.vue
+++ resources/js/vue-components/events/next.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="component next-event-component" v-if="event">
-    <h1>Next</h1>
+    <h1>Next Meeting</h1>
     <div class="box">
         <div class="columns">
             <div class="eventName column is-two-thirds">{{event.name}}</div>
             <div class="eventOrganizer column is-one-third"><span>Organizer: {{event.creator}}</span></div>
         </div>
-        <div class="columns">
-            <div class="eventStart column is-half">Starts: {{event.startTime | date}}({{event.startTime | from }})</div>
-            <div class="eventEnd column is-half">Ends: {{event.endTime | date}}</div>
+        <div>
+            <div class="eventStart"><strong>Starts:</strong> {{event.startTime | date}} ({{event.startTime | from }})</div>
+            <div class="eventEnd"><strong>Ends:</strong> {{event.endTime | date}}</div>
         </div>
     </div>
 </div>
