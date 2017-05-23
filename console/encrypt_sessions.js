f46ef62b9b32cfc9bd9be9da020983875546d2ad
Conner McCall <sloped@sloped.me>
2017-05-23 17:28:21 -0500

    enabling column encryption for google oauth tokens

---
+++ console/encrypt_sessions.js
@@ -0,0 +1,3 @@
+require('babel-register')();
+require('./_encrypt_sessions.es6');
+
--- database/models/users.js
+++ database/models/users.js
@@ -1,6 +1,7 @@
 import get_calendar from './methods/get_calendar.js';
 module.exports = {
     tableName: 'users',
-    getCalendar: get_calendar
-    
-}
+    getCalendar: get_calendar,
+    encryptedColumns: ['google_access_token', 'google_refresh_token', 'googleId'],
+
+};
